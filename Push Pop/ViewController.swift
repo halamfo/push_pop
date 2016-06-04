c5518723048fb96a882c5fb5a5cd57a537953bfd
halamfo <tungnt991@gmail.com>
2016-06-04 15:13:43 +0700

    Initial Commit

---
+++ Push Pop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Push Pop
+//
+//  Created by Dân Tơi on 6/4/16.
+//  Copyright © 2016 Dân Tơi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
